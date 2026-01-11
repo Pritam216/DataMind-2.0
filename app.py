26ffc3262441bffcbf7a659c068cb2b400f31188
Pritam <kumarroypritam2@gmail.com>
2026-01-12 01:57:01 +0530

    DataMind backend 1.0.1 ready

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app = FastAPI(title="DataMind EDA API", version="2.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://datamind-agent.vercel.app/"], 
+    allow_origins=["https://datamind-agent.vercel.app"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
